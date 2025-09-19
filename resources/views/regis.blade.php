20b003325336d30e13deabeca3b09b294aa2b436
omsentod <rifqiwidyadana6@gmail.com>
2025-09-19 15:15:27 +0700

    ++

--- resources/views/regis.blade.php
+++ resources/views/regis.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>Register</title>
     <link rel="stylesheet" href="{{ asset('css/regis.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEJr+M7Lr19s4FgjJt0FJ93b3Vh6h90w4gJz5lD6n2zJjlHj4E0hx/9F5b93z" crossorigin="anonymous">
